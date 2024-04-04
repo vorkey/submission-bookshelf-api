1d642a388b024ff3130196d132e6214cbd7cb4a2
Ambadaya Gaman <ambadayaakun@gmail.com>
2024-04-04 15:02:22 +0700

    addBook and getAll

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: ,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: ,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: ,
+  },
+];
+
+module.exports = routes;
